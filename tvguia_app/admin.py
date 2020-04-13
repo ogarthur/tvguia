3daaf758a13c210908ffd0b6a30b198ffe63f260
Arturo <olmos.arthur@gmail.com>
2020-04-13 19:50:52 +0200

    Added basic css and estructure html. Added basic panel structure

--- tvguia_app/admin.py
+++ tvguia_app/admin.py
@@ -1,7 +1,20 @@
 from django.contrib import admin
 
 # Register your model here.
+from .model.canal import Canal
+from .model.deporte import Deporte
+from .model.documental import Documental
+from .model.serie import Serie, Episodio
+from .model.otro import Otro
+from .model.pelicula import Pelicula
+from .model.noticias import Noticias
+from .model.programa import Programa
 
-#admin.site.register(Bar)
-
-
+admin.site.register(Canal)
+admin.site.register(Deporte)
+admin.site.register(Documental)
+admin.site.register([Serie, Episodio])
+admin.site.register(Otro)
+admin.site.register(Pelicula)
+admin.site.register(Noticias)
+admin.site.register(Programa)
